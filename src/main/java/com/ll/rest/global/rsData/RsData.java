ee785419a670e9de9a5652e4bbf08c25d859d0a8
Okman-0920 <nohoksun94@gmail.com>
2025-01-03 11:49:01 +0900

    RsData 4 <T>

--- src/main/java/com/ll/rest/global/rsData/RsData.java
+++ src/main/java/com/ll/rest/global/rsData/RsData.java
@@ -8,10 +8,10 @@ import lombok.Getter;
 // null인 필드는 JSON에 포함되지 않음
 @AllArgsConstructor
 @Getter
-public class RsData {
+public class RsData<T> { // <T> 는 어떤 타입이든 사용 가능하다
     private String resultCode;
     private String msg;
-    private Object data;
+    private T data;
 
     public RsData(String resultCode, String msg) {
         this(resultCode, msg, null);
