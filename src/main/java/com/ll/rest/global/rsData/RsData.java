d379cad437a9ba591ed93f4d61773e211b6d071e
Okman-0920 <nohoksun94@gmail.com>
2025-01-03 11:27:34 +0900

    RsData 2 JsonInclude(~.NON.NULL)

--- src/main/java/com/ll/rest/global/rsData/RsData.java
+++ src/main/java/com/ll/rest/global/rsData/RsData.java
@@ -1,16 +1,19 @@
 package com.ll.rest.global.rsData;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
+// null인 필드는 JSON에 포함되지 않음
 @AllArgsConstructor
 @Getter
 public class RsData {
     private String resultCode;
     private String msg;
-    private long data;
+    private Long data;
 
     public RsData(String resultCode, String msg) {
-        this(resultCode, msg, 0);
+        this(resultCode, msg, null);
     }
 }
